e16a790c53ab28c100685ec1aa79e5e6116c0342
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-20 18:03:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,22 @@
 class Song
+
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist
+    @artists = artist
     @genre = genre
+
+  end
+
+  def self.name
+    @@name
+  end
+
+  def self.artist
+    @@artist
+  end
+
+  def self.genre
+    @@genre
   end
 
 
