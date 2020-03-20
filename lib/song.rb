3d069da5645823ecbbeb1654648959577d7183b0
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-20 17:55:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,46 +1,4 @@
 class Song
-  attr_accessor :name, :artist, :genre
-
-    @@count = 0
-    @@genre = []
-    @@artist = []
-
-    def initialize(name, artist, genre)
-      @name = song_name
-      @artist = artist
-      @genre = genre
-      @@count += 1
-      @@genres << genre
-      @@artists << artist
+  def initialize(name, artist, genre)
   end
-
-  def self.count
-    @@count
-  end
-
-  def self.genre
-    @@genre.uniq
-  end
-
-  def self.artist
-    @@artist.uniq
-  end
-
-  def self.genre_count
-  genre_count= {}
-  @@genre.each do |genre|
-    if genre_count[genre]
-        genre_count[genre] += 1
-    else
-      genre_count[genre] = 1
-    end
-  end
-  genre_count
-end
-
-def self.artist_count
-    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-  end
-
-
 end
