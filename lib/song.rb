d705b64e50acabc7a8434c8928e91cea50808cb4
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-20 18:17:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
 
   def self.genre_count
     genre_count = Hash.new(0)
-    @@genres.each {|genre| artist_count[genre] + 1}
+    @@genres.each {|genre| genre_count[genre] + 1}
     artist_count
   end
 
